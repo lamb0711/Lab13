a47add0ca77fb00b11fd5b470eb0a474a2387527
Yang sujin <21500394@handong.edu>
2018-04-26 11:07:37 +0900

    instantiation Friend class

--- src/main/java/edu/handong/csee/java/lab13/prob1/InstanceOf.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/InstanceOf.java
@@ -3,10 +3,12 @@ package edu.handong.csee.java.lab13.prob1;
 public class InstanceOf {
 	
 	public static void WhatFriend(Friend friend) {
-		
+		if(friend instanceof ClassFriend)
+			
 	}
 
 	public static void main(String[] args) {
+		Friend friend1 = new Friend();
 
 	}
 
