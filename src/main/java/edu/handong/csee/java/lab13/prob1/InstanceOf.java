5f3d3f6eecd7e1bb763697dff3489e195d3f3c82
Yang sujin <21500394@handong.edu>
2018-05-01 15:27:30 +0900

    move main method to prob1

--- src/main/java/edu/handong/csee/java/lab13/prob1/InstanceOf.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/InstanceOf.java
@@ -15,17 +15,5 @@ public class InstanceOf {
 			
 	}
 
-	public static void main(String[] args) {
-		Friend friend1 = new Friend();
-		SchoolFriend friend2 = new SchoolFriend();
-		ClassFriend friend3 = new ClassFriend();
-		
-		InstanceOf.whatFriend(friend1);
-		InstanceOf.whatFriend(friend2);
-		InstanceOf.whatFriend(friend3);
-		
-		
-
-	}
 
 }
--- src/main/java/edu/handong/csee/java/lab13/prob1/Prob1.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Prob1.java
@@ -3,7 +3,13 @@ package edu.handong.csee.java.lab13.prob1;
 public class Prob1 {
 
 	public static void main(String[] args) {
+		Friend friend1 = new Friend();
+		SchoolFriend friend2 = new SchoolFriend();
+		ClassFriend friend3 = new ClassFriend();
 		
+		InstanceOf.whatFriend(friend1);
+		InstanceOf.whatFriend(friend2);
+		InstanceOf.whatFriend(friend3);
 
 	}
 
