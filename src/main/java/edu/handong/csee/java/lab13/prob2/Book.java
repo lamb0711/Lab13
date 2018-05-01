99eb67726098dfb41d66504a69948cfbdeb5edb7
Yang sujin <21500394@handong.edu>
2018-05-01 23:00:12 +0900

    arrange source file

--- src/main/java/edu/handong/csee/java/lab13/prob2/Book.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/Book.java
@@ -4,7 +4,7 @@ public class Book {//Book class
 	private static int idCount = 0;//declare static variable idCount
 	private String bookName;//save book name string
 	private int id;//save id number
-	
+
 	public Book(String name) {//Book constructor. it accept book name string
 		this.bookName = name;//save input data to book name
 		idCount++;//add idCount integer variable
