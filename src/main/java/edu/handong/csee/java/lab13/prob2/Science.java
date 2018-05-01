e37fcdc7785354eaa58dc5a300159228168a0b6f
Yang sujin <21500394@handong.edu>
2018-05-01 22:12:41 +0900

    make constructor perfect

--- src/main/java/edu/handong/csee/java/lab13/prob2/Science.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/Science.java
@@ -2,7 +2,8 @@ package edu.handong.csee.java.lab13.prob2;//package name
 
 public class Science extends Book {//Science class. it inheritance Book class
 	private String publisher;//declare private variable
-	public Science(String name, String publisher) {
-		
+	public Science(String name, String publisher) {//constructor. it input name and publisher string data 
+		super(name);//name save to Book class
+		this.publisher = publisher;//input data save publisher of class variable
 	}
 }
