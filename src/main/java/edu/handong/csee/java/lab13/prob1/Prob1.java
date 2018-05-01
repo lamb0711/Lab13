d9767fd1682a4c45eb3db5d5ebf26ab0019dc2d8
Yang sujin <21500394@handong.edu>
2018-05-01 20:46:28 +0900

    change of to in

--- src/main/java/edu/handong/csee/java/lab13/prob1/Prob1.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Prob1.java
@@ -7,9 +7,9 @@ public class Prob1 {//main class Prob1
 		SchoolFriend friend2 = new SchoolFriend();//instantiation SchoolFriend class
 		ClassFriend friend3 = new ClassFriend();//instantiation ClassFriend class
 
-		InstanceOf.whatFriend(friend1);//call class method whatFrend of InstanceOf class
-		InstanceOf.whatFriend(friend2);//call class method whatFrend of InstanceOf class
-		InstanceOf.whatFriend(friend3);//call class method whatFrend of InstanceOf class
+		InstanceOf.whatFriend(friend1);//call class method whatFrend in InstanceOf class
+		InstanceOf.whatFriend(friend2);//call class method whatFrend in InstanceOf class
+		InstanceOf.whatFriend(friend3);//call class method whatFrend in InstanceOf class
 
 	}
 
