eca850ec7bb689c6eb8f265cb1b8441fe88d53f4
Yang sujin <21500394@handong.edu>
2018-05-02 01:21:24 +0900

    add line comments and arrange Main class

--- src/main/java/edu/handong/csee/java/lab13/prob3/Main.java
+++ src/main/java/edu/handong/csee/java/lab13/prob3/Main.java
@@ -1,28 +1,28 @@
-package edu.handong.csee.java.lab13.prob3;
+package edu.handong.csee.java.lab13.prob3;//package name
 
-import java.util.Scanner;
+import java.util.Scanner;//use scanner class
 
-public class Main {
+public class Main {//Main class
 
-	public static void main(String[] args) {
-		double n1,n2;
-		Scanner keyboard = new Scanner(System.in);
-		
-		System.out.print("Enter radius: ");
-		n1 = keyboard.nextDouble();
-		Circle c1 = new Circle(n1);
-		
-		System.out.println("Radius: " + c1.getRadius());
-		c1.display();
-		
-		System.out.print("Enter length and width: ");
-		n1 = keyboard.nextDouble();
-		n2 = keyboard.nextDouble();
-		Rectangle r1 = new Rectangle(n1,n2);
-		
-		System.out.println("Length: " + r1.getLength());
-		System.out.println("Width: " + r1.getWidth());
-		r1.display();
+	public static void main(String[] args) {//main method
+		double n1,n2;//double variable n1, n2
+		Scanner keyboard = new Scanner(System.in);//use scanner class to use keyboard
+
+		System.out.print("Enter radius: ");//print enter message
+		n1 = keyboard.nextDouble();//save input data to n1
+		Circle c1 = new Circle(n1);//instantiation Circle class to c1
+
+		System.out.println("Radius: " + c1.getRadius());//print radius to use getRadius method
+		c1.display();//display result to use display method
+
+		System.out.print("Enter length and width: ");//print message to input data
+		n1 = keyboard.nextDouble();//save input data to n1
+		n2 = keyboard.nextDouble();//save input data to n2
+		Rectangle r1 = new Rectangle(n1,n2);//instantiation Rectangle class to r1
+
+		System.out.println("Length: " + r1.getLength());//print Length to use getLength method
+		System.out.println("Width: " + r1.getWidth());//print Width to use getWidth method
+		r1.display();//display result to use display method
 
 	}
 
