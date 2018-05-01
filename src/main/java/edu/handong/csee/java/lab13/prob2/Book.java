e500352842bbf1c057a8d05dc17c239c3f460440
Yang sujin <21500394@handong.edu>
2018-05-01 21:32:23 +0900

    add line comment in variable

--- src/main/java/edu/handong/csee/java/lab13/prob2/Book.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/Book.java
@@ -1,5 +1,12 @@
-package edu.handong.csee.java.lab13.prob2;
+package edu.handong.csee.java.lab13.prob2;//package name
 
-public class Book {
+public class Book {//Book class
+	private static int idCount = 0;//declare static variable idCount
+	private String bookName;//save book name string
+	private int id;//save id number
+	public Book(String name) {//Book constructor. it accept book name string
+		
+		
+	}
 
 }
