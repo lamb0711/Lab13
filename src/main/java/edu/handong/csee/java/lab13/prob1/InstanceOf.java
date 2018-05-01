035ccd3018ecd45f53a890cdcc3b48595848288a
Yang sujin <21500394@handong.edu>
2018-05-01 21:02:42 +0900

    add comments

--- src/main/java/edu/handong/csee/java/lab13/prob1/InstanceOf.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/InstanceOf.java
@@ -1,17 +1,17 @@
-package edu.handong.csee.java.lab13.prob1;
+package edu.handong.csee.java.lab13.prob1;//package name
 
-public class InstanceOf {
+public class InstanceOf {//class name 
 	
-	public static void whatFriend(Friend friend) {
+	public static void whatFriend(Friend friend) {//whatFriend is static method! and it accept data to Friend class type
 		
-		if(friend instanceof ClassFriend)
-			( (ClassFriend)friend).classFriend();
+		if(friend instanceof ClassFriend)//if friend can change to ClassFriend class type, return true 
+			( (ClassFriend)friend).classFriend();//change friend to ClassFriend class type and call classFriend method
 		
-		else if(friend instanceof SchoolFriend)
-				( (SchoolFriend)friend).schoolFriend();
+		else if(friend instanceof SchoolFriend)//if friend can change to SchoolFriend class type, return true 
+				( (SchoolFriend)friend).schoolFriend();//change friend to SchoolFriend class type and call schoolFriend method
 		
-		else
-			friend.justFriend();
+		else//if friend can't change to ClassFriend and SchoolFriend class type, return true 
+			friend.justFriend();//call classFriend method in Friend class
 			
 	}
 
