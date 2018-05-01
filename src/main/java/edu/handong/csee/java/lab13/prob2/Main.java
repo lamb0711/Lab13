97de03d9fadbe6b75efbe260c64b6abefdd7050a
Yang sujin <21500394@handong.edu>
2018-05-01 22:38:14 +0900

    instantiation Science class and input data

--- src/main/java/edu/handong/csee/java/lab13/prob2/Main.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/Main.java
@@ -3,6 +3,7 @@ package edu.handong.csee.java.lab13.prob2;//package name
 public class Main {//Main class
 	
 	public static void main(String[] args) {//main method
-		Book book = new Book("Simple Book");
+		Book book = new Book("Simple Book");//instantiation Book class and input data;
+		Science science = new Science("Hello Physics!", "ScienceWorld");//instantiation Science class and input data
 	}
 }
