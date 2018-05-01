03a2f8b1931561704a0bab190c1ed79b0f930c20
Yang sujin <21500394@handong.edu>
2018-05-01 22:36:33 +0900

    make main method and instantiation Book class. input new data

--- src/main/java/edu/handong/csee/java/lab13/prob2/Main.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/Main.java
@@ -1,5 +1,8 @@
-package edu.handong.csee.java.lab13.prob2;
+package edu.handong.csee.java.lab13.prob2;//package name
 
-public class Main {
-
+public class Main {//Main class
+	
+	public static void main(String[] args) {//main method
+		Book book = new Book("Simple Book");
+	}
 }
