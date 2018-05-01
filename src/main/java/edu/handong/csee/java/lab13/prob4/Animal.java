5514b44dde7ccaed30c4d83177e03acb07878236
Yang sujin <21500394@handong.edu>
2018-05-02 01:58:51 +0900

    make Animal class and line comments

--- src/main/java/edu/handong/csee/java/lab13/prob4/Animal.java
+++ src/main/java/edu/handong/csee/java/lab13/prob4/Animal.java
@@ -1,5 +1,13 @@
-package edu.handong.csee.java.lab13.prob4;
+package edu.handong.csee.java.lab13.prob4;//package name
 
-public class Animal {
+public class Animal {//Animal class
+	private String name;//save animal name
 
+	public void setName(String name) {//setting input data to name
+		this.name = name;//save input data name to name class variable
+	}
+
+	public void getName() {//print name data
+		System.out.println("Name: "+name);//print name
+	}
 }
