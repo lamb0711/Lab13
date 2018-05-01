6a77c322d2c53f8df3bcb724fce27471d1fcd99b
Yang sujin <21500394@handong.edu>
2018-05-01 22:25:04 +0900

    change line comments

--- src/main/java/edu/handong/csee/java/lab13/prob2/History.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/History.java
@@ -1,5 +1,9 @@
 package edu.handong.csee.java.lab13.prob2;//package name
 
 public class History extends Book{//History class. it inheritance Book class
-
+	private String author;//save author name
+	
+	public History(String name, String author) {//History constructor. it inheritance Book class
+		
+	}
 }
--- src/main/java/edu/handong/csee/java/lab13/prob2/Science.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/Science.java
@@ -1,6 +1,6 @@
 package edu.handong.csee.java.lab13.prob2;//package name
 
-public class Science extends Book {//Science class. it inheritance Book class
+public class Science extends Book {//Science constructor. it inheritance Book class
 	private String publisher;//declare private variable
 	public Science(String name, String publisher) {//constructor. it input name and publisher string data 
 		super(name);//name save to Book class
