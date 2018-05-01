87b55d199856bc53eadd1a5c20ebe384fe1b983a
Yang sujin <21500394@handong.edu>
2018-05-01 15:44:26 +0900

    line comments

--- src/main/java/edu/handong/csee/java/lab13/prob1/Prob1.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Prob1.java
@@ -1,15 +1,15 @@
-package edu.handong.csee.java.lab13.prob1;
+package edu.handong.csee.java.lab13.prob1;//package name
 
-public class Prob1 {
+public class Prob1 {//main class Prob1
 
-	public static void main(String[] args) {
-		Friend friend1 = new Friend();
-		SchoolFriend friend2 = new SchoolFriend();
-		ClassFriend friend3 = new ClassFriend();
+	public static void main(String[] args) {//main method
+		Friend friend1 = new Friend();//instantiation Friend class
+		SchoolFriend friend2 = new SchoolFriend();//instantiation SchoolFriend class
+		ClassFriend friend3 = new ClassFriend();//instantiation ClassFriend class
 		
-		InstanceOf.whatFriend(friend1);
-		InstanceOf.whatFriend(friend2);
-		InstanceOf.whatFriend(friend3);
+		InstanceOf.whatFriend(friend1);//call class method whatFrend of InstanceOf class
+		InstanceOf.whatFriend(friend2);//call class method whatFrend of InstanceOf class
+		InstanceOf.whatFriend(friend3);//call class method whatFrend of InstanceOf class
 
 	}
 
