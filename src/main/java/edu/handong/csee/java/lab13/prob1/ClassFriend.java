1c2a9d9f355331d25ef445f610e93aa3135b8f48
Yang sujin <21500394@handong.edu>
2018-05-01 21:22:43 +0900

    line comments

--- src/main/java/edu/handong/csee/java/lab13/prob1/ClassFriend.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/ClassFriend.java
@@ -1,8 +1,8 @@
-package edu.handong.csee.java.lab13.prob1;
+package edu.handong.csee.java.lab13.prob1;//package name
 
-public class ClassFriend extends SchoolFriend{
-	public void classFriend() {
-		System.out.println("Class Frend");
+public class ClassFriend extends SchoolFriend{//ClassFriend class. it inheritance SchoolFriend class
+	public void classFriend() {//classFriend class
+		System.out.println("Class Friend");//print Class Friend
 	}
 
 }
