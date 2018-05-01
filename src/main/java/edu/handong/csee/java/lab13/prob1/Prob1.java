e4ea7de9162e3c88f159b5676e5f269ca881217d
Yang sujin <21500394@handong.edu>
2018-05-01 15:44:49 +0900

    arrange Prob1 class

--- src/main/java/edu/handong/csee/java/lab13/prob1/Prob1.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Prob1.java
@@ -6,7 +6,7 @@ public class Prob1 {//main class Prob1
 		Friend friend1 = new Friend();//instantiation Friend class
 		SchoolFriend friend2 = new SchoolFriend();//instantiation SchoolFriend class
 		ClassFriend friend3 = new ClassFriend();//instantiation ClassFriend class
-		
+
 		InstanceOf.whatFriend(friend1);//call class method whatFrend of InstanceOf class
 		InstanceOf.whatFriend(friend2);//call class method whatFrend of InstanceOf class
 		InstanceOf.whatFriend(friend3);//call class method whatFrend of InstanceOf class
