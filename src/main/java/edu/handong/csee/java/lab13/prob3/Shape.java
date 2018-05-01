b5cbae71f469eaecbd2435a88f6e029fe0f9f956
Yang sujin <21500394@handong.edu>
2018-05-02 01:02:50 +0900

    add line comments and arrange Shape class

--- src/main/java/edu/handong/csee/java/lab13/prob3/Shape.java
+++ src/main/java/edu/handong/csee/java/lab13/prob3/Shape.java
@@ -1,10 +1,10 @@
-package edu.handong.csee.java.lab13.prob3;
+package edu.handong.csee.java.lab13.prob3;//package name
 
-public abstract class Shape {
-	public abstract double area();
-	public abstract double perimeter();
-	public void display() {
-		System.out.println("Area: "+area()+ "\nPerimeter" + perimeter()+ "\n");
+public abstract class Shape {//abstract class Shape. It have method of heading. it don't need instantiation
+	public abstract double area();//It is abstract method so it must declare in subclass
+	public abstract double perimeter();//It is abstract method so it must declare in subclass
+	public void display() {//display method
+		System.out.println("Area: "+area()+ "\nPerimeter" + perimeter()+ "\n");//print area and perimeter
 	}
 
 }
