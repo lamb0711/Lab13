772bdf3603c7157441ba66ccb98e23766c763b2f
Yang sujin <21500394@handong.edu>
2018-05-02 01:49:09 +0900

    add Pet interface and line comments

--- src/main/java/edu/handong/csee/java/lab13/prob4/Pet.java
+++ src/main/java/edu/handong/csee/java/lab13/prob4/Pet.java
@@ -1,5 +1,5 @@
-package edu.handong.csee.java.lab13.prob4;
+package edu.handong.csee.java.lab13.prob4;//package name
 
-public interface Pet {
-
+public interface Pet {//Pet is interface. if some classes implements this, classes has horizontal relation 
+	public String Food();//if classes implements Pet, they must input this method
 }
