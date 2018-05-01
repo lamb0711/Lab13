10da2114bd24ce3d3210450c59062778896c81ce
Yang sujin <21500394@handong.edu>
2018-05-01 22:08:47 +0900

    declare variable and constructor

--- src/main/java/edu/handong/csee/java/lab13/prob2/Science.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/Science.java
@@ -1,5 +1,8 @@
-package edu.handong.csee.java.lab13.prob2;
+package edu.handong.csee.java.lab13.prob2;//package name
 
-public class Science {
-
+public class Science extends Book {//Science class. it inheritance Book class
+	private String publisher;//declare private variable
+	public Science(String name, String publisher) {
+		
+	}
 }
