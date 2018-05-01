9c9a8756a2157e17ba63a4f14031bdca4f684398
Yang sujin <21500394@handong.edu>
2018-05-02 00:38:26 +0900

    make methods in Circle class

--- src/main/java/edu/handong/csee/java/lab13/prob3/Circle.java
+++ src/main/java/edu/handong/csee/java/lab13/prob3/Circle.java
@@ -1,5 +1,21 @@
 package edu.handong.csee.java.lab13.prob3;
 
 public class Circle extends Shape{
+	private double radius;
+	public Circle(double r) {
+		radius = r;
+	}
+	
+	public double area() {
+		return Math.PI * Math.pow(radius, 2);
+	}
+	
+	public double perimeter() {
+		return 2.0 * Math.PI*radius;
+	}
+	
+	public double getRadius() {
+		return radius;
+	}
 
 }
