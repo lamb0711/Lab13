2687f106d68d3ecd7808edd5110ffde440f715d1
Yang sujin <21500394@handong.edu>
2018-05-02 00:41:20 +0900

    use scanner class

--- src/main/java/edu/handong/csee/java/lab13/prob3/Main.java
+++ src/main/java/edu/handong/csee/java/lab13/prob3/Main.java
@@ -1,8 +1,13 @@
 package edu.handong.csee.java.lab13.prob3;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		double n1,n2;
+		Scanner keyboard = new Scanner(System.in);
+		
 		
 
 	}
