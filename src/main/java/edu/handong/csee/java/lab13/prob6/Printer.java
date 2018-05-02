89fabf725f5f4903e4bc5ee7845d81f8f24818e7
Yang sujin <21500394@handong.edu>
2018-05-02 12:38:42 +0900

    add line comments

--- src/main/java/edu/handong/csee/java/lab13/prob6/Printer.java
+++ src/main/java/edu/handong/csee/java/lab13/prob6/Printer.java
@@ -1,11 +1,11 @@
 package edu.handong.csee.java.lab13.prob6;//package name
 
 public class Printer {//Printer class
-	public static void print(Object object) {
-		String str = object.toString();
-		if(object instanceof CapitalPrint)
-			str = str.toUpperCase();
-		System.out.println(str);
+	public static void print(Object object) {//static method print. it accept input data to Object type
+		String str = object.toString();//change object variables to string and save str
+		if(object instanceof CapitalPrint)//if object implement CapitalPrint interface, implements it
+			str = str.toUpperCase();//change string to upper case and save it to str
+		System.out.println(str);//print str
 	}
 
 }
