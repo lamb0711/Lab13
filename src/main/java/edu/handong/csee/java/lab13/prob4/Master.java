7008d50cf8df626f24c47a30b75b0c483571f6ff
Yang sujin <21500394@handong.edu>
2018-05-02 11:10:03 +0900

    make main method and variables

--- src/main/java/edu/handong/csee/java/lab13/prob4/Master.java
+++ src/main/java/edu/handong/csee/java/lab13/prob4/Master.java
@@ -1,8 +1,23 @@
-package edu.handong.csee.java.lab13.prob4;
+package edu.handong.csee.java.lab13.prob4;//package name
+
+import java.util.Scanner;
 
 public class Master {
 	public void eatFeed(Pet pet) {
-		System.out.println("feed: "+pet.Food());
+		System.out.println("feed: "+pet.returnFood());
+	}
+	
+	public static void main(String[] args) {
+		String catName, dogName;
+		
+		Master master = new Master();
+		Cat cat = new Cat();
+		Dog dog = new Dog();
+		
+		Scanner keyboard = new Scanner(System.in);
+		System.out.print("Enter the cat name and dog name! ");
+		catName = keyboard.next();
+		dogName = keyboard.next();
 	}
 
 }
