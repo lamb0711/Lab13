fb90d51a832079550a0d6a49da469b190cdd10cf
Yang sujin <21500394@handong.edu>
2018-05-02 11:07:07 +0900

    add Dog class and line comments

--- src/main/java/edu/handong/csee/java/lab13/prob4/Dog.java
+++ src/main/java/edu/handong/csee/java/lab13/prob4/Dog.java
@@ -1,5 +1,8 @@
-package edu.handong.csee.java.lab13.prob4;
+package edu.handong.csee.java.lab13.prob4;//package name
 
-public class Dog extends Animal implements Pet{
+public class Dog extends Animal implements Pet{//Dog inherits class Animal and interface Pet 
+	public String returnFood() {//returnFood method. it must conclude
+		return "(Dog!)" + "Sausage";//return String
+	}
 
 }
