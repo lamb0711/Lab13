601d83d8c3f7403687f7e09877ca72365f1b853f
Yang sujin <21500394@handong.edu>
2018-05-02 11:03:11 +0900

    add line comments in Cat

--- src/main/java/edu/handong/csee/java/lab13/prob4/Cat.java
+++ src/main/java/edu/handong/csee/java/lab13/prob4/Cat.java
@@ -1,5 +1,8 @@
-package edu.handong.csee.java.lab13.prob4;
+package edu.handong.csee.java.lab13.prob4;//package name
 
-public class Cat extends Animal implements Pet{
+public class Cat extends Animal implements Pet{//Cat inherits class Animal and interface Pet 
+	public String returnFood() {//returnFood method. it must conclude
+		return "(Cat!)" + "Fish";//return String
+	}
 
 }
