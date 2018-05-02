b5e9452f668a2e9f263c86e5c224710ce7a38a23
Yang sujin <21500394@handong.edu>
2018-05-02 12:58:23 +0900

    add comments to UpPoint class

--- src/main/java/edu/handong/csee/java/lab13/prob6/UpPoint.java
+++ src/main/java/edu/handong/csee/java/lab13/prob6/UpPoint.java
@@ -1,15 +1,15 @@
-package edu.handong.csee.java.lab13.prob6;
+package edu.handong.csee.java.lab13.prob6;//package name
 
-public class UpPoint implements CapitalPrint{
-	private int x, y;
-	
-	UpPoint(int x,int y){
-		this.x = x;
-		this.y = y;
+public class UpPoint implements CapitalPrint{//UpPoint class it use interface CapitalPrint
+	private int x, y;//x and y integer variable
+
+	UpPoint(int x,int y){//constructor UpPoint
+		this.x = x;//save input data to x
+		this.y = y;//save input data to y
 	}
-	
-	public String toString() {
-		return "x : " + x + " y : " + y;
+
+	public String returnString() {//returnString class return String
+		return "x : " + x + " y : " + y;//return string
 	}
 
 }
